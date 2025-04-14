63f865cf664213cb03747446d60da7ee979ca7d3
Wouter Stultiens <wouter.stultiens@nl.abnamro.com>
2025-04-14 20:15:05 +0200

    add post and basemodel

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 def read_root():
@@ -10,3 +14,7 @@ def read_root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+@app.post("/createpost")
+def create_post(post: Post):
+    return {"new_post": f"title: {post.title}, content: {post.content}"}
