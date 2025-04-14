d964370c3c3d4ac665e4f711e9e8632d8fe0bf2e
Wouter Stultiens <wouter.stultiens@nl.abnamro.com>
2025-04-14 20:49:23 +0200

    store new posts in memory

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [
+    {"title": "Exploring the Stars", "content": "Join us as we delve into the mysteries of the cosmos and uncover the secrets of distant galaxies.", "id": 1},
+    {"title": "The Art of Cooking", "content": "Discover the joy of culinary creations with our step-by-step guides to mastering delicious recipes.", "id": 2}
+]
 
 @app.get("/")
 def read_root():
@@ -13,8 +23,11 @@ def read_root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createpost")
+@app.post("/posts")
 def create_post(post: Post):
-    return {"new_post": f"title: {post.title}, content: {post.content}"}
+    post_dict = dict(post)
+    post_dict["id"] = randrange(0, 9999999)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
